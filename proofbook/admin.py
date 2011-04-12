6385533761bae7ee79450cbeb4857bae2354656b
Josh <Josh@PCsForEducation.com>
2011-04-12 16:05:44 -0500

    Reorganizing into the standard App Engine/Django directory structure. Also added in the updated settings from other projects.

--- proofbook/admin.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.contrib import admin
-from models import Album, Site, Category
-
-admin.site.register(Album)
-admin.site.register(Site)
-admin.site.register(Category)
